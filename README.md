2ac9c3caaecc0ec559fd4de50e3169f35487927b
Chase Matua <chase.matua@mx.com>
2022-12-02 00:14:17 -0700

    Gotta throw a party for my day ones - Drake

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# advent-twentytwo
+# Advent of Code 2022
+Solving puzzles, collecting stars, adventuring with elves.
+
+![legolas-gif](https://i.gifer.com/Q0D9.gif)
+This is Santa's Top Elf.
+
+*[Advent of Code home page](https://adventofcode.com/)*
---
+++ first/first.py
@@ -0,0 +1,23 @@
+# Advent of Code - 2022, Day 1: Calorie Counting
+
+# Opening input and creating calorie_list
+with open('calorie_input_list.txt') as f:
+    calorie_list = f.read()
+
+# Bunch'a packs carried by elves separated by two new lines
+packs = calorie_list.split("\n\n")
+
+# Sum of calories in each elf's pack
+elves = []
+for pack in packs:
+    elf = sum(eval(item) for item in pack.split('\n'))
+    elves.append(elf)
+
+# Most calories to least calories
+elves.sort(reverse=True)
+
+# Part I
+print("The elf carrying the most calories is carrying", max(elves), "calories.")
+
+# Part II
+print("The top three elves carrying the most calories are carrying", sum(elves[:3]), "calories altogether.")
